863c508adc0177459c2868146c3a9c68dda9d309
Christian Gram Kalhauge <christiankalhauge@gmail.com>
2024-02-27 16:26:25 +0100

    Adding onGlitterEach and onGlitterEachWith

--- src/Test/Hspec/Glitter.hs
+++ src/Test/Hspec/Glitter.hs
@@ -7,7 +7,9 @@
 module Test.Hspec.Glitter (
   -- * Glitter
   onGlitter,
+  onGlitterEach,
   onGlitterWith,
+  onGlitterEachWith,
 ) where
 
 -- hspec
@@ -17,8 +19,9 @@ import Test.Hspec.Core.Spec
 
 -- hspec-glitter
 import Control.Git
+import Control.Monad (zipWithM_)
+import Data.Function
 
--- | Given a folder or file, run a command
 onGlitterWith
   :: (HasCallStack)
   => FilePath
@@ -37,7 +40,6 @@ onGlitterWith fp runner spec = do
           [] -> Success
           xs -> Failure location (Reason $ unlines $ "The following files have changed:" : xs)
 
--- | Given a folder or file, run a command
 onGlitter
   :: (HasCallStack)
   => FilePath
@@ -48,3 +50,41 @@ onGlitter
   -- ^ The tests to run on the changed files.
   -> SpecWith a
 onGlitter fp runner = onGlitterWith fp (const runner)
+
+onGlitterEachWith
+  :: (HasCallStack)
+  => FilePath
+  -- ^ The file or folder to monitor
+  -> (a -> FilePath -> IO ())
+  -- ^ The command to populate the folder or file with data.
+  -> SpecWith FilePath
+  -- ^ The tests to run on the changed files.
+  -> SpecWith a
+onGlitterEachWith fp runner spec = do
+  describe fp do
+    beforeAllWith (\a -> runner a fp >> gitChanges fp) do
+      _ <-
+        spec & mapSpecItem_ \item ->
+          item
+            { itemExample = \pr han pc ->
+                pc & itemExample item pr \ac ->
+                  han \cs ->
+                    let n = length cs
+                     in zipWithM_ (\i c -> pc (i, n) >> ac c) [0 ..] cs
+            }
+
+      it "should not have changed" $ \changes -> do
+        Result "" $ case changes of
+          [] -> Success
+          xs -> Failure location (Reason $ unlines $ "The following files have changed:" : xs)
+
+onGlitterEach
+  :: (HasCallStack)
+  => FilePath
+  -- ^ The file or folder to monitor
+  -> (FilePath -> IO ())
+  -- ^ The command to populate the folder or file with data.
+  -> SpecWith FilePath
+  -- ^ The tests to run on the changed files.
+  -> SpecWith a
+onGlitterEach fp runner = onGlitterEachWith fp (const runner)
