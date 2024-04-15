5c8d175018fdb4cf9ca5f9bda2cd32da5a414ce0
Christian Gram Kalhauge <christiankalhauge@gmail.com>
2024-04-15 12:49:05 +0200

    Adding somthing to the readme and golden tests

--- README.md
+++ README.md
@@ -1,7 +1,32 @@
 # Not all that G[l]itters is Gold.
 
-A Git based golden test helper.
+The idea is simple. Instead of predicting the output of a test, you simply run
+the test, and save the results. Now, you can check if the test have changed,
+according to the version control system.
 
+# Features
+
+Seperately from other packages, in Glitter test. 
+
+1. There are no seperate tool for accepting and inspecting golden test. You can
+   simply use git, as you would normally.
+
+1. After running the golden test, you can run a sequence of checks to see if
+   the saved file is well-formed. This way you don't save files without some
+   minimal assurances.
+
+# Missing Features
+
+Eventually, I would like to add:
+
+1. Templating. Normalizing output is hard and not always straight forward, when
+   dates or other things are involved. In this case, it would be nice if the
+   golden file, is a template that can simply check if it matches.
+
+1. Force test mode. In a CI/CD system, you would like to test all cases, even
+   the ones that havn't changed. Adding a flag to controll this would be nice.
+
+1. Inline diffing.
 
 # Related Packages
 
--- src/Test/Hspec/Glitter.hs
+++ src/Test/Hspec/Glitter.hs
@@ -5,6 +5,10 @@
 {-# LANGUAGE TypeFamilies #-}
 
 module Test.Hspec.Glitter (
+  -- * Golden
+  golden,
+  goldenWith,
+
   -- * Glitter
   onGlitter,
   onGlitterEach,
@@ -13,15 +17,37 @@ module Test.Hspec.Glitter (
 ) where
 
 -- hspec
-
 import Test.Hspec.Core.Hooks
 import Test.Hspec.Core.Spec
 
 -- hspec-glitter
 import Control.Git
+
+-- base
 import Control.Monad (zipWithM_)
 import Data.Function
 
+-- | A simple golden test. Fails if the content of the FilePath have changed.
+golden
+  :: (HasCallStack)
+  => FilePath
+  -> (FilePath -> IO ())
+  -> Spec
+golden fp fn = onGlitter fp fn (pure ())
+
+{- | A simple golden test (given an input argument).
+Fails if the content of the FilePath have changed.
+-}
+goldenWith
+  :: (HasCallStack)
+  => FilePath
+  -> (a -> FilePath -> IO ())
+  -> SpecWith a
+goldenWith fp fn = onGlitterWith fp fn (pure ())
+
+{- | Given a filepath, a function to populate that folder, and a
+specification tree with a list of changed filepaths.
+-}
 onGlitterWith
   :: (HasCallStack)
   => FilePath
@@ -51,6 +77,7 @@ onGlitter
   -> SpecWith a
 onGlitter fp runner = onGlitterWith fp (const runner)
 
+-- | Like @onGlitterWith@ but maps over the changed files one by one.
 onGlitterEachWith
   :: (HasCallStack)
   => FilePath
@@ -61,23 +88,17 @@ onGlitterEachWith
   -- ^ The tests to run on the changed files.
   -> SpecWith a
 onGlitterEachWith fp runner spec = do
-  describe fp do
-    beforeAllWith (\a -> runner a fp >> gitChanges fp) do
-      _ <-
-        spec & mapSpecItem_ \item ->
-          item
-            { itemExample = \pr han pc ->
-                pc & itemExample item pr \ac ->
-                  han \cs ->
-                    let n = length cs
-                     in zipWithM_ (\i c -> pc (i, n) >> ac c) [0 ..] cs
-            }
+  onGlitterWith fp runner $
+    spec & mapSpecItem_ \item ->
+      item
+        { itemExample = \pr handler pc ->
+            pc & itemExample item pr \ac ->
+              handler \cs ->
+                let n = length cs
+                 in zipWithM_ (\i c -> pc (i, n) >> ac c) [0 ..] cs
+        }
 
-      it "should not have changed" $ \changes -> do
-        Result "" $ case changes of
-          [] -> Success
-          xs -> Failure location (Reason $ unlines $ "The following files have changed:" : xs)
-
+-- | onGlitterEachWith but without a spec argument.
 onGlitterEach
   :: (HasCallStack)
   => FilePath
